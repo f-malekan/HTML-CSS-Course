5890ff05022872090501f8efd318dd05d72d31b4
Fateme <fateme.malekan96@gmail.com>
2023-05-09 19:02:49 +0430

    collapsibles added.

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const collapsibles = document.querySelectorAll('.collapsible')
+console.log(collapsibles)
+collapsibles.forEach((item)=>{
+    item.addEventListener('click',function(){
+        this.classList.toggle('collapsible--expanded')
+    })
+})
